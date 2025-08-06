cc240a49b0b62fe13b197a775292fa30bca66f37
khuram shahzad <khurramitdeveloper1@gmail.com>
2025-08-06 13:30:42 +0500

    Its just a comment

--- ChatNest/Program.cs
+++ ChatNest/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:44313");
 
 builder.Services.AddControllersWithViews();
-
+//its new
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
