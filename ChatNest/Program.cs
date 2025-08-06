3a9c0ad454d39c01759e909f9c662350f3134653
khuram shahzad <khurramitdeveloper1@gmail.com>
2025-08-06 13:35:47 +0500

    new change

--- ChatNest/Program.cs
+++ ChatNest/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:44313");
 
 builder.Services.AddControllersWithViews();
-//its newgggg
+//its n
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
